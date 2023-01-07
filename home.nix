279ef0d5632c62817d8d83412a3383e115eaddf1
Simon Zeng <contact@simonzeng.com>
2023-01-07 12:45:41 -0500

    Initial commit

---
+++ home.nix
@@ -0,0 +1,119 @@
+{ config, pkgs, ... }:
+
+# github.com/fmoda3/nix-configs: home/nvim/
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "kronicmage";
+  home.homeDirectory = "/home/kronicmage";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # no config programs
+  programs.pywal.enable = true;
+  programs.kitty.enable = true;
+
+  home.packages = with pkgs; [
+    grim
+    slurp
+  ];
+
+  programs.git = {
+    enable = true;
+    package = pkgs.gitAndTools.gitFull;
+    userName = "Simon Zeng";
+    userEmail = "contact@simonzeng.com";
+    extraConfig = {
+      core.editor = "nvim";
+      credential.helper = "store";
+    };
+    signing = {
+      key = "973C9963CA528797";
+      signByDefault = true;
+    };
+
+  };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    withPython3 = true;
+
+    plugins = with pkgs.vimPlugins; [
+      # feline-nvim
+      # focus-nvim
+      # gitsigns-nvim
+      # legendary.nvim
+      # nvim-autopairs
+      # telescope-ui-select-nvim
+      # vim-koka
+      # (nvim-treesitter.withPlugins
+      #   (plugins: pkgs.nvim-ts-grammars.allGrammars)
+      # )
+      comment-nvim
+      coq-artifacts
+      coq_nvim
+      dhall-vim
+      dressing-nvim
+      gruvbox-nvim
+      haskell-tools-nvim
+      haskell-vim
+      indent-blankline-nvim
+      leap-nvim
+      lsp-status-nvim
+      lsp_extensions-nvim
+      lspkind-nvim
+      nvim-colorizer-lua
+      nvim-lspconfig
+      nvim-tree-lua
+      nvim-treesitter
+      nvim-treesitter-context
+      nvim-treesitter-textobjects
+      nvim-ts-rainbow
+      nvim-web-devicons
+      parinfer-rust
+      plenary-nvim
+      purescript-vim
+      stabilize-nvim
+      telescope-fzy-native-nvim
+      telescope-nvim
+      telescope-symbols-nvim
+      vim-endwise
+      vim-repeat
+      vim-sexp
+      vim-sexp-mappings-for-regular-people
+      vim-surround
+      vimagit
+      vimtex
+      vista-vim
+      which-key-nvim
+    ];
+  };
+
+  xdg.configFile.nvim = {
+    source = ./nvim;
+    recursive = true;
+  };
+
+  wayland.windowManager.sway = {
+    enable = true;
+  };
+
+  xdg.configFile.sway = {
+    source = ./sway;
+    recursive = true;
+  };
+}
---
+++ nvim/lua/options.lua
@@ -0,0 +1,45 @@
+local options = {
+  autoindent=true;
+  backspace="indent,eol,start";
+  completeopt="menuone,noinsert,noselect";
+  cursorline=true;
+  expandtab=true;
+  foldexpr="nvim_treesitter#foldexpr()";
+  foldlevel=99;
+  -- foldmethod="expr";
+  foldmethod="indent";
+  guifont="JetBrainsMono Nerd Font Mono:h13";
+  hidden=true;
+  hlsearch=true;
+  inccommand="split";
+  laststatus = 3;
+  lazyredraw=true;
+  linebreak=true;
+  mouse="nvi";
+  number=true;
+  pastetoggle="<F2>";
+  relativenumber=true;
+  scrolloff=999999;
+  shada="'20,<50,s10";
+  shiftwidth=4;
+  shortmess="aoOTIcF";
+  showmatch=true;
+  sidescrolloff=5;
+  signcolumn="number";
+  smartcase=true;
+  smarttab=true;
+  splitbelow=true;
+  splitright=true;
+  tabstop=4;
+  termguicolors=true;
+  timeout=true;
+  timeoutlen=300;
+  -- updatetime=300;
+  virtualedit="block";
+  wrap=false;
+}
+
+
+for name, value in pairs(options) do
+  vim.o[name] = value
+end
