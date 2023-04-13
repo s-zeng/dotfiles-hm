f96ce73f2956453650b6a23afb4f5adc02e14fbb
Simon Zeng <contact@simonzeng.com>
2023-04-13 00:08:47 -0400

    Update config.nix

--- config.nix
+++ config.nix
@@ -2,4 +2,11 @@
   useWayland = true;
   thinkpad = false;
   graphical = false;
+  allowUnfree = true;
+  packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
+
 }
