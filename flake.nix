49ffe4a2c2e445c33ff8aa374d72b905e5bf75ae
Simon Zeng <contact@simonzeng.com>
2023-06-17 23:05:47 -0400

    Attempt to flakify part 6

--- flake.nix
+++ flake.nix
@@ -16,7 +16,19 @@
       system = "x86_64-linux";  # x86_64-linux, aarch64-multiplatform, etc.
       stateVersion = "22.11";     # See https://nixos.org/manual/nixpkgs/stable for most recent
 
-      config = import ./config.nix;
+      config =
+        # mega ugly copy paste of system/config.nix cause idk how to import this
+        {
+          useWayland = false;
+          thinkpad = true;
+          graphical = true;
+          allowUnfree = true;
+          packageOverrides = pkgs: {
+            nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+              inherit pkgs;
+            };
+          };
+        };
 
       pkgs = import nixpkgs {
         inherit system;
