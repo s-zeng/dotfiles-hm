b2839dfe23fba410ccd635db4a6f7dbb83c82527
Simon Zeng <contact@simonzeng.com>
2023-07-06 00:54:48 -0400

    Fix nvim lsp keybinds

--- nvim/lua/plugin/lsp.lua
+++ nvim/lua/plugin/lsp.lua
@@ -3,24 +3,20 @@ local nvim_lsp = require('lspconfig')
 
 lsp_status.register_progress()
 
-local attach_hook = function(status_callback)
-  return function(client, bufnr)
-    vim.g["coq_settings"] = { auto_start = 'shut-up' }
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-    status_callback(client)
-
-    local opts = { noremap=true, silent=true, buffer = bufnr }
+    local opts = { noremap = true, buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', '<leader>la', vim.lsp.buf.range_code_action, opts)
     vim.keymap.set('n', '<leader>lc', vim.lsp.codelens.run, opts)
     vim.keymap.set('n', '<leader>lS', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>lf', function () vim.lsp.buf.format{ async = true } end, opts)
+    vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, opts)
     -- vim.keymap.set('v', '<leader>lF', vim.lsp.formatexpr({},{0,0},{vim.fn.line("$"),0}), opts)
     vim.keymap.set('n', '<leader>ld', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, opts)
@@ -32,10 +28,11 @@ local attach_hook = function(status_callback)
     vim.keymap.set('n', ']c', vim.diagnostic.goto_next, opts)
     vim.keymap.set('n', '<leader>E', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', '<leader>e', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', '<leader>lt', require'lsp_extensions'.inlay_hints, opts)
-    vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, { pattern="<buffer>", callback = vim.diagnostic.open_float})
-  end
-end
+    vim.keymap.set('n', '<leader>lt', require 'lsp_extensions'.inlay_hints, opts)
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" },
+      { pattern = "<buffer>", callback = vim.diagnostic.open_float })
+  end,
+})
 
 local configs = require 'lspconfig/configs'
 local util = require 'lspconfig/util'
@@ -63,8 +60,8 @@ local default_config_servers = {
 
 for _, lsp in ipairs(default_config_servers) do
   nvim_lsp[lsp].setup {
-    on_attach = attach_hook(lsp_status.on_attach),
-    capabilities=lsp_status.capabilities,
+    on_attach = lsp_status.on_attach,
+    capabilities = lsp_status.capabilities,
     flags = {
       debounce_text_changes = 150,
     }
@@ -73,25 +70,25 @@ end
 
 local ht = require('haskell-tools')
 
-local opts = { noremap = true, silent = true, buffer = bufnr }
 ht.setup {
   hls = {
     -- See nvim-lspconfig's  suggested configuration for keymaps, etc.
     on_attach = function(client, bufnr)
+      local opts = { noremap = true, silent = true, buffer = bufnr }
       -- haskell-language-server relies heavily on codeLenses,
       -- so auto-refresh (see advanced configuration) is enabled by default
       vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
-      attach_hook(lsp_status.on_attach)(client, bufnr)  -- if defined, see nvim-lspconfig
+      lsp_status.on_attach(client, bufnr) -- if defined, see nvim-lspconfig
     end,
   },
 }
 
 nvim_lsp['clangd'].setup {
-  on_attach = attach_hook(lsp_status.extensions.clangd.setup),
-  capabilities=lsp_status.capabilities,
-    flags = {
-      debounce_text_changes = 150,
-    }
+  on_attach = lsp_status.extensions.clangd.setup,
+  capabilities = lsp_status.capabilities,
+  flags = {
+    debounce_text_changes = 150,
+  }
 }
 
 require('lspkind').init()
