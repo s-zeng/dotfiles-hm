9f2eddf9c35ce3f2cd0783c07f6ae5c40a400d4d
Simon Zeng <contact@simonzeng.com>
2023-07-09 03:49:20 -0400

    Add some mini stuff

--- nvim/lua/plugin/mini.lua
+++ nvim/lua/plugin/mini.lua
@@ -2,16 +2,10 @@ require('mini.comment').setup()
 require('mini.fuzzy').setup()
 require('mini.files').setup()
 require('mini.pairs').setup()
-local hipatterns = require('mini.hipatterns')
+require('mini.move').setup()
+local hipatterns = require('mini.hipatterns').setup()
 hipatterns.setup({
   highlighters = {
-    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
-    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
-    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
-
-    -- Highlight hex color strings (`#rrggbb`) using that color
     hex_color = hipatterns.gen_highlighter.hex_color(),
   },
 })
