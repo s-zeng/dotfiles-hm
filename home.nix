fd04d21e41f24c0b594c11114c3097e883e45b9f
Simon Zeng <contact@simonzeng.com>
2025-08-06 14:37:27 +0100

    Add helix

--- home.nix
+++ home.nix
@@ -242,6 +242,23 @@ in
         ocamlPackages.ocamlformat-rpc-lib
       ];
     };
+
+    helix = {
+        enable = true;
+        settings = {
+          theme = "gruvbox";
+          editor = {
+            line-number = "relative";
+            lsp.display-messages = true;
+          };
+          # keys.normal = {
+          #   space.space = "file_picker";
+          #   space.w = ":w";
+          #   space.q = ":q";
+          #   esc = [ "collapse_selection" "keep_primary_selection" ];
+          # };
+        };
+    };
   } // (if graphical then {
 
     wezterm = {
