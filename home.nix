f1972cdcbba880a0ddb07cec8ea05c7fcf06c08f
Simon Zeng <contact@simonzeng.com>
2025-11-14 10:35:50 +0000

    more performant jj starship

--- home.nix
+++ home.nix
@@ -231,30 +231,31 @@ in
         custom = {
           jj = {
             description = "The current jj status";
-            when = "jj --ignore-working-copy root";
+            detect_folders = [".jj"];
             symbol = "🥋 ";
+            shell = ["sh" "--noprofile" "--norc"];
             command = "jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template 'separate(\" \", change_id.shortest(4), bookmarks, \"|\", concat(if(conflict, \"💥\"), if(divergent, \"🚧\"), if(hidden, \"👻\"), if(immutable, \"🔒\")), raw_escape_sequence(\"\\x1b[1;32m\") ++ if(empty, \"(empty)\"), raw_escape_sequence(\"\\x1b[1;32m\") ++ coalesce(truncate_end(29, description.first_line(), \"…\"), \"(no description set)\") ++ raw_escape_sequence(\"\\x1b[0m\"))'";
           };
           git_status = {
-            when = "! jj --ignore-working-copy root";
+            detect_folders = ["!.jj"];
             command = "starship module git_status";
             style = "";
             description = "Only show git_status if we're not in a jj repo";
           };
           git_commit = {
-            when = "! jj --ignore-working-copy root";
+            detect_folders = ["!.jj"];
             command = "starship module git_commit";
             style = "";
             description = "Only show git_commit if we're not in a jj repo";
           };
           git_metrics = {
-            when = "! jj --ignore-working-copy root";
+            detect_folders = ["!.jj"];
             command = "starship module git_metrics";
             style = "";
             description = "Only show git_metrics if we're not in a jj repo";
           };
           git_branch = {
-            when = "! jj --ignore-working-copy root";
+            detect_folders = ["!.jj"];
             command = "starship module git_branch";
             style = "";
             description = "Only show git_branch if we're not in a jj repo";
