00a87179a064213ffdbea507094f55f880ee9088
Simon Zeng <contact@simonzeng.com>
2025-12-28 12:42:46 +0000

    log

--- home.nix
+++ home.nix
@@ -309,7 +309,10 @@ in
           diff-invocation-mode = "file-by-file";
         };
         revset-aliases."immutable_heads()" = "builtin_immutable_heads() | (trunk().. & ~mine()) | bookmarks(glob:rel_*) | bookmarks(glob:prod_*)";
-        aliases.tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
+        aliases = {
+          tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
+          l = ["log"];
+        };
         merge-tools.diffconflicts = {
           program = "nvim";
           merge-args = [
