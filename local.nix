a059ba5daf3b7f90bba7699bebe0ae049716cdb2
Simon Zeng <contact@simonzeng.com>
2026-03-04 10:17:54 +0000

    local.nix

---
+++ local.nix
@@ -0,0 +1,12 @@
+{
+  username = "simonzeng";
+  name = "Simon Zeng";
+  email = "contact@simonzeng.com";
+  system = "aarch64-darwin";
+  config = {
+    useWayland = false;
+    thinkpad = false;
+    graphical = true;
+    allowUnfree = true;
+  };
+}
